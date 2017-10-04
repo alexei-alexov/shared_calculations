f9734907efdb16fedb97e7b06f91e405c4e95ffb
alexei-alexov <olexii.lykianchyk@gmail.com>
2017-10-05 01:07:10 +0300

    Done lab1, working on lab2

---
+++ lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+
+namespace lab1
+{
+    class ThreadTest
+    {
+        static void Main(string[] args)
+        {
+            Thread thr = new Thread(WriteY);
+            thr.Start();
+            while(true)
+                Console.Write("X");
+        }
+
+        static void WriteY()
+        {
+            while(true)
+                Console.Write("Y");
+        }
+    }
+}
+
---
+++ lab2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program {
+
+        static void Main(string[] args) {
+            int amount = GetPositiveInt("Enter amount of data: ");
+
+            
+            Console.WriteLine("Hello World!");
+        }
+
+        static int GetPositiveInt(string prompt) {
+            while (true) {
+                try {
+                    Console.WriteLine(promt);
+                    string data = Console.ReadLine();
+
+                    int result = Int32.Parse(data);
+                    if (result < 1)
+                        throw new ArgumentException("Number must be greater than 0");
+
+                    return result;
+                }
+                catch(FormatException) {
+                    Console.WriteLine("Bad format for {0}", data);
+                    return -1;
+                }
+                catch(OverflowException) {
+                    Console.WriteLine("Too big number {0}", data);
+                    return -1;
+                }
+                catch(Exception e) {
+                    Console.WriteLine("Error: {0}", e.Message);
+                    return -1;
+                }
+            }
+        }
+    }
+}
